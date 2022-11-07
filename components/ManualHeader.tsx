bc8cdf08ecbbd175d0c2aab985a245057edc5cc4
Ali <ali.salloum.gh@gmail.com>
2022-11-07 13:56:20 +0300

    added prettier and morlis and reformatted the code

---
+++ components/ManualHeader.tsx
@@ -0,0 +1,7 @@
+import { useMoralis } from "react-moralis"
+
+export default function ManualHeader() {
+    const { enableWeb3 } = useMoralis()
+
+    return <div></div>
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,16 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head"
+import styles from "../styles/Home.module.css"
+import Header from "../components/ManualHeader"
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Decentralized Lottery</title>
-        <meta name="description" content="Decentralized Lottery built with hardhat" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-    </div>
-  )
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Decentralized Lottery</title>
+                <meta name="description" content="Decentralized Lottery built with hardhat" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Header />
+        </div>
+    )
 }
